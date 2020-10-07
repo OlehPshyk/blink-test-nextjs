1e92c77eee50a173c37700b97a928d674e8eec1a
Oleh Pshyk <pshyk.oleg@gmail.com>
2020-10-07 04:18:29 +0300

    first step

---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import classNames from 'classnames';
+
+// import shippingStyles from '../styles/shipping.module.scss'
+import headerStyles from '../styles/header.module.scss'
+import mSectionStyles from '../styles/mSection.module.scss'
+
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Blink test</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={headerStyles.header}>
+        <div className={headerStyles.logoContainer}>
+          <img src="images/logo.svg" className={headerStyles.logo}/>
+        </div>
+        <nav className={headerStyles.nav}>
+          <Link href="#">
+            <a>Vai al sito</a>
+          </Link>
+        </nav>
+      </header>
+      <main className="main">             
+        <section className={mSectionStyles.mSection}>
+          <div className={mSectionStyles.mSectionBg}>
+            <div className={classNames(mSectionStyles.mSectionContent,"wrapper")}>
+              <span className={mSectionStyles.t1}>SPEDIZIONE N° 1234567890</span>
+              <div className={mSectionStyles.bottomText}>
+                <h2 className={mSectionStyles.t2}>Ciao <span className={mSectionStyles.t21}>Marco</span></h2>
+                <span className={mSectionStyles.t3}>La tua spedizione è stata</span>
+                <h3 className={mSectionStyles.t4}>IN CONSEGNA</h3>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      {/* <footer className="footer"></footer> */}
+    </React.Fragment>
+  )
+}
