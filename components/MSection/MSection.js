c208c4a508290d26c082d2155c2418b65295a87c
Oleh Pshyk <pshyk.oleg@gmail.com>
2020-10-08 18:59:13 +0300

    data

--- components/MSection/MSection.js
+++ components/MSection/MSection.js
@@ -2,16 +2,16 @@ import classNames from 'classnames';
 
 import mSectionStyles from './mSection.module.scss';
 
-export default function MSection() {
+export default function MSection({code,status,firstName}) {
   return (        
     <section className={mSectionStyles.mSection}>
       <div className={mSectionStyles.mSectionBg}>
         <div className={classNames(mSectionStyles.mSectionContent,"wrapper")}>
-          <span className={mSectionStyles.t1}>SPEDIZIONE N° 1234567890</span>
+          <span className={mSectionStyles.t1}>SPEDIZIONE N° {code}</span>
           <div className={mSectionStyles.bottomText}>
-            <h2 className={mSectionStyles.t2}>Ciao <span className={mSectionStyles.t21}>Marco</span></h2>
+            <h2 className={mSectionStyles.t2}>Ciao <span className={mSectionStyles.t21}>{firstName}</span></h2>
             <span className={mSectionStyles.t3}>La tua spedizione è stata</span>
-            <h3 className={mSectionStyles.t4}>IN CONSEGNA</h3>
+            <h3 className={mSectionStyles.t4}>{status}</h3>
           </div>
         </div>
       </div>
